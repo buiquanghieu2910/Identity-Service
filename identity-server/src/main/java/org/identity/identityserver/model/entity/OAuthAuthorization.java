41973e740f76629609fbc6df9f9d88718b234fda
buiquanghieu2910 <bui.quang.hieu2910@gmail.com>
2025-05-24 01:40:23 +0700

    Update

--- identity-server/src/main/java/org/identity/identityserver/model/entity/OAuthAuthorization.java
+++ identity-server/src/main/java/org/identity/identityserver/model/entity/OAuthAuthorization.java
@@ -1,6 +1,9 @@
 package org.identity.identityserver.model.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.hibernate.annotations.DynamicInsert;
